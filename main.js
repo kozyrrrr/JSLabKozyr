5626560b9ece9c0d43cc48c173233c71d60a7bfd
kozyrrrr <kozyrstudy@gmail.com>
2022-02-17 01:54:50 +0300

    final
    v3

--- main.js
+++ main.js
@@ -54,48 +54,104 @@ typeof(2 != 1)
 // Фактический: number
 
 // Задача №2 Прямоугольник
-console.log("------------------------");
-let a = 2;
-let b = 3;
-console.log(a,b);
-console.log(2*(a+b));
-console.log(a*b);
-console.log((2*(a+b))/(a*b));
-console.log("------------------------");
-//Задача №3 Конвертация единиц измерения
-let degreesC = 25;
-let degreesF = 66.2;
-console.log(degreesC +"°C" +" "+ "соответствует" + " " + (degreesC*(9/5)+32) +"°F" )
-console.log(degreesF +"°F" +" "+ "соответствует" + " " + Math.round(((degreesF-32)*(5/9))) +"°C" )
-console.log("------------------------");
+// let a = 2;
+// let b = 3;
+// console.log(a,b);
+// console.log(2*(a+b));
+// console.log(a*b);
+// console.log((2*(a+b))/(a*b));
+// //Задача №3 Конвертация единиц измерения
+// let degreesC = 25;
+// let degreesF = 66.2;
+// console.log(degreesC +"°C" +" "+ "соответствует" + " " + (degreesC*(9/5)+32) +"°F" )
+// console.log(degreesF +"°F" +" "+ "соответствует" + " " + Math.round(((degreesF-32)*(5/9))) +"°C" )
 // Задача №4 Високосный год
 // let date = prompt("Введите год", "Год");
 // !(date % 4) ? (!(date %100) ? alert(true): alert(false)): alert(false)
 // Задача №5 Проверка
-let num1 = 3;
-let num2 = 10;
-if ((num1 == 10 || num2 == 10) || num1+num2==10 ) console.log(true);
-console.log("------------------------");
+// let num1 = 3;
+// let num2 = 10;
+// if ((num1 == 10 || num2 == 10) || num1+num2==10 ) console.log(true);
 // Задача №6 Считаем овец
 // let userNumber = prompt("Введите число", "число");
-// let i=1;
-// console.log(
-//     while(i<=userNumber)
-// );
+// let i=2;
+// oldAnswer = 1 + " " + "овечка";
+// while (i <= userNumber){
+//     answer = oldAnswer + "..." + i + " " + "овечка" + "...";
+//    oldAnswer = answer;
+//    i++;
+// }
+// console.log(answer);
 // Задача №7 Четное-нечетное
-for (let i =0;i<=15;i++){
-    if (i % 2 == 0) {
-        console.log(i + " " + "четное");
-    }
-    else {
-        console.log(i + " " + "нечетное");
-    }
+// for (let i = 0;i <= 15;i++){
+//     if (i % 2 == 0) {
+//         console.log(i + " " + "четное");
+//     }
+//     else {
+//         console.log(i + " " + "нечетное");
+//     }
+// }
+// // Задача №8 Елка к новому году
+// ВАРИАНТ 1 без разделения строк
+// let i = 1,  char1 = '*', char2 = '##';
+// for(i ; i <= 6; i++){
+//     console.log(char1);
+//     char1 = char1 + "*" + "*";
+//     console.log(char2);
+//     char2 = char2 + "#" + "#";
+// }
+// ВАРИАНТ 2 с разделением строки
+let i = 0 , j = 0, str = "", char1 = '*', char2 = '##';
+for (i;i <= 5;i++) {
+    for (j;j <= i;j++) {
+        str = str + char1 + "\n" + char2 + "\n";
+    } 
+    char2 = char2 + "#" + "#";
+    char1 = char1 + "*" + "*";
 }
-console.log("------------------------");
-// Задача №8 Елка к новому году
-for (let i = 1; i<7;i++){
-    for (let j =2; j<8;j++){
-        console.log()
-    }
-}
-
+console.log(str);
+//  Задача №9 Сортировка
+// let num1 = -15;
+// let num2 = -12;
+// let num3 = 2;
+// let maxNum, midlleNum, minNum;
+// if (num1>num2){
+//     midlleNum = num1;
+//     minNum = num2;
+// } else{
+//     midlleNum = num2;
+//     minNum = num1;
+// }
+// if (midlleNum>num3){
+//     maxNum =  midlleNum;
+//     if(minNum>num3){
+//         midlleNum = minNum;
+//         minNum=num3;
+//     } else{
+//         midlleNum = num3;
+// } 
+// } else maxNum = num3;
+// console.log(minNum, midlleNum,maxNum); // опустим момент с одинаковыми числами
+// Задача №10 Максимальное значение
+// let num1 = 2, num2 = -3, num3 = 4, num4 = -6, num5 = 9, maxNum;
+// if (num1>num2 && num1>num3 && num1>num4 && num1>num5)
+// {
+//   maxNum = num1;
+// }
+// else if (num2>num1 && num2>num3 && num2>num4 && num2>num5)
+// {
+//     maxNum = num2;
+// }
+// else if (num3>num1 && num3>num2 && num3>num4 && num3>num5)
+// {
+//     maxNum = num3;
+// }
+// else if (num4>num1 && num4>num2 && num4>num3 && num4>num5)
+// {
+//     maxNum = num4;
+// }
+// else
+// {
+//     maxNum = num5;
+// }
+// console.log(maxNum);
